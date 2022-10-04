8b9880c100e9539912e6346e484636c8fa7e34b6
Sniky83 <sniky83@hotmail.fr>
2022-10-04 15:46:26 +0200

    Premier test unitaire sur premier controller en GET

--- TestsWebAPI/SelfieControllerUnitTest.cs
+++ TestsWebAPI/SelfieControllerUnitTest.cs
@@ -1,11 +1,23 @@
+using SelfieAWookieAPI.Controllers;
+
 namespace TestsWebAPI
 {
     public class SelfieControllerUnitTest
     {
+        #region Public Methods
         [Fact]
-        public void Test1()
+        public void ShouldReturnListOfSelfies()
         {
+            // Arrange
+            var controller = new SelfiesController();
 
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.GetEnumerator().MoveNext());
         }
+        #endregion
     }
 }
